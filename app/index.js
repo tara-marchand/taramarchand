380844d1a36d8fcef6db56d8e6604ad8d0404c54
Tara Marchand <tara@mac.com>
2017-09-10 13:06:52 -0700

    fix port # for Heroku

--- app/index.js
+++ app/index.js
@@ -34,6 +34,6 @@ const renderIndex = function(req, res) {
 }
 app.get('/*', renderIndex)
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('App listening on port 3000.');
 })
