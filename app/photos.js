3531f19857c72ed9e8f95df5666a5be42593174f
Tara Marchand <tara@mac.com>
2018-04-23 21:25:26 -0700

    wip

--- app/photos.js
+++ app/photos.js
@@ -1,5 +1,8 @@
+import cloudinary from 'cloudinary';
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
+
+const getFilenameFromUrl = (url) => url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
 
 class Photos {
   index(req, res) {
@@ -24,7 +27,24 @@ class Photos {
             const edges =
               window._sharedData.entry_data.ProfilePage[0].graphql.user
                 .edge_owner_to_timeline_media.edges;
-            const srcs = edges.map(edge => edge.node.thumbnail_src);
+            // filenames
+            const srcs = edges.map(edge => {
+              const filename = getFilenameFromUrl(edge.node.thumbnail_src);
+              const imageUrl = `http://res.cloudinary.com/hlvbevjxx/${filename}`;
+
+              cloudinary.v2.uploader.upload(
+                edge.node.thumbnail_src,
+                { public_id: getFilenameFromUrl(edge.node.thumbnail_src) },
+                function(error, result) {
+                  console.log(result);
+                }
+              );
+
+              return 
+          }
+          // check for file's existence in cloudinary
+            // if exists, return
+            // if does not exist, upload, return
 
             res
               .status(200)
