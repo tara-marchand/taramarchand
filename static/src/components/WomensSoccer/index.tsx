b4b54052e81a9060f44b56300b2fef3827be5aa1
Tara Marchand <tara@mac.com>
2018-12-30 11:21:00 -0800

    Soccer thing

---
+++ static/src/components/WomensSoccer/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as competitions from '../../../../node_modules/open-data/data/competitions';
+
+import Competition from './Competition';
+
+export interface Props {}
+
+export default class WomensSoccer extends React.PureComponent<Props, any> {
+  public competitions: WomensSoccerTypes.Competition[] = competitions.default;
+
+  public render() {
+    return <div className="WomensSoccer">
+        {this.competitions.map((competition) => {
+            const {competition_name: name, country_name: country} = competition;
+            
+            return <Competition competition_name={name} country_name={country} />
+        })}
+    </div>;
+  }
+}
