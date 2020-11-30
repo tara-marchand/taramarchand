3a8b6b198c09ca308c444b931d22b93274f20c89
Tara Marchand <tmarchand@newrelic.com>
2020-11-30 10:26:34 -0800

    Do not purge

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
   important: false,
   plugins: [],
   prefix: '',
-  purge: [
-    '../views/**/*.hbs',
-    '../static/src/**/*.jsx',
-    '../static/src/**/*.tsx',
-  ],
   separator: ':',
   theme: {
     extend: {
