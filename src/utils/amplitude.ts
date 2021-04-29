2f54c6fe2fcf809d98419c80d3b45481073a8026
Tara Marchand <tara@mac.com>
2021-04-28 21:34:38 -0700

    More tweaks

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,15 +1,11 @@
-import { AmplitudeClient } from 'amplitude-js';
+import amplitude, { AmplitudeClient } from 'amplitude-js';
 
 let amplitudeInstance: AmplitudeClient | undefined;
 
 if (window && process.env.NODE_ENV === 'production') {
-  import('amplitude-js').then((amp) => {
-    const ampApiKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
-    if (!ampApiKey) {
-      return;
-    }
-console.log(ampApiKey)
-    amplitudeInstance = amp.getInstance(ampApiKey);
+  const ampApiKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
+  if (ampApiKey) {
+    amplitudeInstance = amplitude.getInstance(ampApiKey);
     amplitudeInstance.init(ampApiKey, undefined, {
       includeUtm: true,
       includeReferrer: true,
@@ -17,7 +13,7 @@ console.log(ampApiKey)
 
     const identify = new amplitudeInstance.Identify();
     amplitudeInstance.identify(identify);
-  });
+  }
 }
 
 export { amplitudeInstance };
