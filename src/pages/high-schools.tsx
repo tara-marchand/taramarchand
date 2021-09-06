21533f8a0a14e224196bb0569f9c0107eae35116
Tara Marchand <tara@mac.com>
2021-09-06 13:27:18 -0700

    Schools WIP

--- src/pages/high-schools.tsx
+++ src/pages/high-schools.tsx
@@ -1,10 +1,13 @@
 import Airtable from 'airtable';
+import { noop } from 'lodash';
 import React, { ReactElement } from 'react';
+import EasyEdit from 'react-easy-edit';
 
 const airtableHsSearchBase = Airtable.base('appxkLGn7WpgCoCbM');
 
 type HighSchool = {
   name: string;
+  notes?: string;
 };
 
 interface ServerSideProps {
@@ -20,6 +23,7 @@ export async function getServerSideProps() {
 
   const highSchools = highSchoolRecords.map((record) => ({
     name: record.fields.school,
+    notes: record.fields.notes,
   }));
 
   return JSON.parse(JSON.stringify({ props: { highSchools } }));
@@ -30,7 +34,21 @@ export default function HighSchools(props: ServerSideProps): ReactElement {
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-6">
         {props.highSchools?.map((school) => (
-          <div key={school.name}>{school.name}</div>
+          <div className="overflow-hidden shadow-lg" key={school.name}>
+            <div className="px-6 pt-4">
+              <div className="mb-2 font-bold">{school.name}</div>
+            </div>
+            <div className="px-6 pb-4">
+              <EasyEdit
+                cancelButtonLabel="Cancel"
+                onCancel={() => noop()}
+                onSave={() => noop()}
+                saveButtonLabel="Save"
+                type="textarea"
+                value={school.notes}
+              />
+            </div>
+          </div>
         ))}
       </div>
     </div>
