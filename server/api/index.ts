858b185eb70daf175fbc8459a585ef2e5c5ae204
Tara Marchand <tara@mac.com>
2021-11-14 16:29:01 -0800

    Archive some currently unused modules

--- server/api/index.ts
+++ server/api/index.ts
@@ -2,12 +2,12 @@ import { FastifyPluginCallback, RawServerBase } from 'fastify';
 import fp from 'fastify-plugin';
 
 import { contact } from './contact';
-import { sms } from './sms';
+// import { sms } from '../api_archive/sms';
 
 // define plugin using callbacks
 const api = fp(async function (fastify, _options, done) {
   fastify.register(contact);
-  fastify.register(sms);
+  // fastify.register(sms);
   done();
 } as FastifyPluginCallback<Record<string, unknown>, RawServerBase>);
 
--- server/models/index.ts
+++ server/models/index.ts
@@ -1,9 +1,10 @@
 import { Model, ModelCtor } from 'sequelize-typescript';
 
-import AuthToken from './AuthToken';
-import Book from './Book';
-import User from './User';
+// import AuthToken from '../inactive_models/AuthToken';
+// import Book from '../inactive_models/Book';
+// import User from '../inactive_models/User';
 
-const models: ModelCtor<Model>[] = [AuthToken, Book, User];
+// const models: ModelCtor<Model>[] = [AuthToken, Book, User];
+const models: ModelCtor<Model>[] = [];
 
 export default models;
