97bc1995bc7551342f37ff420abf6c0300652603
Tara Marchand <tara@mac.com>
2022-05-13 20:39:59 -0700

    Fix browser agent

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,32 +1,16 @@
-import Document, {
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
-import { InferGetServerSidePropsType } from 'next/types';
-import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export async function getServerSideProps() {
-  // You must require agent and put it within this function
-  // otherwise it will try to get bundled by webpack and cause errors.
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const newrelic = await import('newrelic');
-  const browserTimingHeader: string = newrelic.getBrowserTimingHeader();
-  return { props: { browserTimingHeader } };
-}
+import { newRelicBrowserAgent } from '../utils/newRelicBrowserAgent';
 
-export default class MyDocument extends Document<InferGetServerSidePropsType<typeof getServerSideProps>> {
+export default class MyDocument extends Document {
   render() {
-    const { browserTimingHeader } = this.props;
-
     return (
       <Html>
-        <Head />
-        {process.env.NEXT_PUBLIC_NODE_ENV === 'production' &&
-          browserTimingHeader && (
-            <div dangerouslySetInnerHTML={{ __html: browserTimingHeader }} />
-          )}
+        <Head>
+          {process.env.NEXT_PUBLIC_NODE_ENV === 'production' && <script
+            dangerouslySetInnerHTML={{ __html: newRelicBrowserAgent }}
+          />}
+        </Head>
         <body className="m-0">
           <Main />
           <NextScript />
