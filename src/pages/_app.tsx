fde3941374faf149d211d9f91ff9013f5be94e99
Tara Marchand <tara@mac.com>
2022-06-13 09:25:47 -0700

    Add umami again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import Layout from '../components/Layout';
 
 import '../styles/globals.css';
@@ -6,6 +7,13 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script
+        async
+        defer
+        data-website-id="b4ce9aed-5080-4035-b668-fef77e91772e"
+        src="https://umami.tmarchand.com/umami.js"
+        strategy="beforeInteractive"
+      ></Script>
       <Component {...pageProps} />
     </Layout>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import { Script } from 'next/script';
 
 import { newRelicBrowserAgent } from '../utils/newRelicBrowserAgent';
 
@@ -8,22 +7,13 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {process.env.NEXT_PUBLIC_NODE_ENV === 'production' && (
-            <script
-              dangerouslySetInnerHTML={{ __html: newRelicBrowserAgent }}
-            />
-          )}
+          {process.env.NEXT_PUBLIC_NODE_ENV === 'production' && <script
+            dangerouslySetInnerHTML={{ __html: newRelicBrowserAgent }}
+          />}
         </Head>
         <body className="m-0">
           <Main />
           <NextScript />
-          <Script
-              async
-              defer
-              data-website-id="b4ce9aed-5080-4035-b668-fef77e91772e"
-              src="https://umami.tmarchand.com/umami.js"
-              strategy="beforeInteractive"
-            ></Script>
         </body>
       </Html>
     );
