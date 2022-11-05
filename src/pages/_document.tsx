ff3220e8fdbb2d4f665142bdeb991eda573f63eb
Tara Marchand <tara@mac.com>
2022-11-05 11:44:37 -0700

    Fix layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className="m-0 h-full">
         <Head />
-        <body className="m-0">
+        <body className="m-0 h-full">
           <Main />
           <NextScript />
         </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import bakerBeach from '../public/images/baker-beach.jpg';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <Image src={bakerBeach} alt="Baker Beach, San Francisco, CA" />
+    <div className="relative h-full">
+      <Image style={{objectFit: 'cover', width: '100%'}} src={bakerBeach} alt="Baker Beach, San Francisco, CA" />
     </div>
   );
 }
