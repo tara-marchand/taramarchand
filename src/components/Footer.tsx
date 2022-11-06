e382f62f2204154ceea732680aafb3d0dcd53824
Tara Marchand <tara@mac.com>
2022-11-06 10:08:48 -0800

    Update footer links, inc. email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,27 @@ const Footer: React.FunctionComponent = () => {
       <div className="mx-auto flex max-w-screen-md px-6 md:px-0">
         <SocialIcon
           className="mr-6"
-          url="https://twitter.com/trmarchand"
+          url=""
+          network="email"
+          onClick={openEmail}
           rel="noopener noreferrer"
           target="_blank"
         />
         <SocialIcon
+          className="mr-6"
           url="https://www.linkedin.com/in/trmarch/"
+          rel="noopener noreferrer"
+          target="_blank"
+        />
+        <SocialIcon
+          className="mr-6"
+          url="https://github.com/tara-marchand"
+          rel="noopener noreferrer"
+          target="_blank"
+        />
+        <SocialIcon
+          className="mr-6"
+          url="https://twitter.com/trmarchand"
           rel="noopener noreferrer"
           target="_blank"
         />
@@ -22,5 +37,16 @@ const Footer: React.FunctionComponent = () => {
     </footer>
   );
 };
+
+function openEmail() {
+    const name = 'tara';
+    const at = '@';
+    const domain = 'mac';
+    const dot = '.';
+    const tld = 'com';
+    const address = name + at + domain + dot + tld;
+    
+    location.href = 'mailto:' + address;
+}
 
 export default Footer;
