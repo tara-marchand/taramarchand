f1a3ea4ad7c870c78c6406caed8d76671c78d661
Tara Marchand <tara@mac.com>
2022-12-20 16:12:35 -0800

    Re-add NR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,43 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const newrelic = require('newrelic');
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
-export default class MyDocument extends Document {
+type CustomDocumentInitialProps = DocumentInitialProps & {
+  browserTimingHeader: string;
+};
+
+export default class MyDocument extends Document<CustomDocumentInitialProps> {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<CustomDocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const browserTimingHeader = newrelic.getBrowserTimingHeader({
+      hasToRemoveScriptWrapper: true,
+    });
+
+    return {
+      ...initialProps,
+      browserTimingHeader,
+    };
+  }
+
   render() {
     return (
       <Html className="m-0 h-full">
-        <Head />
+        <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
+          />
+        </Head>
         <body className="m-0 h-full">
           <Main />
           <NextScript />
