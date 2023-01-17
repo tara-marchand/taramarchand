5b00100bb59623c5400bf916eeb9399841aa73a3
Tara Marchand <tmarchand@newrelic.com>
2023-01-17 12:48:16 -0800

    Remove loki

--- server/logger.ts
+++ server/logger.ts
@@ -1,20 +1,9 @@
 import pino, { Level } from 'pino';
-import createWriteStreamSync from 'pino-loki';
-
-const pinoLokiOptions = {
-  applicationTag: 'taramarchand', // The tag every log file should be logged with
-  host: 'https://loki.tmarchand.com', // Pino loki instance IP address and port
-  labels: {
-    application: 'taramarchand.com',
-  },
-  silenceErrors: false,
-  timeout: 3000, // Set timeout to 3 seconds, default is 30 minutes.
-};
 
 export const getPinoLogger = async (logLevel: Level) =>
   pino(
     {
-      level: 'debug',
+      level: 'info',
       // @see https://github.com/fastify/fastify/blob/69df0e39fa5886fcd8d5411c590a429e16a2c3ae/lib/logger.js#L48
       serializers: {
         req: function asReqValue(req) {
@@ -29,8 +18,4 @@ export const getPinoLogger = async (logLevel: Level) =>
         },
       },
     },
-    // @see https://skaug.dev/node-js-app-with-loki/
-    pino.multistream([
-      { level: logLevel, stream: await createWriteStreamSync(pinoLokiOptions) },
-    ])
   );
