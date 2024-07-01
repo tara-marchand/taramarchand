f8c1ae5b2d956ece6d6e8a4ffaa1e9f69709c0be
Tara Marchand <tara@mac.com>
2024-06-30 17:58:04 -0700

    Add tracing

--- src/faro.ts
+++ src/faro.ts
@@ -1,6 +1,7 @@
 'use client';
 
-import { initializeFaro } from '@grafana/faro-web-sdk';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 initializeFaro({
   url: 'https://alloy.tmarchand.com/collect',
@@ -9,5 +10,14 @@ initializeFaro({
     name: 'taramarchand',
     version: '1.0.0',
   },
+  instrumentations: [
+    ...getWebInstrumentations(),
+    new TracingInstrumentation({
+      instrumentationOptions: {
+        // Requests to these URLs have tracing headers attached.
+        propagateTraceHeaderCorsUrls: [new RegExp('https://www.taramarchand.com/*'), new RegExp('http://localhost:3333/*')],
+      },
+    }),
+  ],
 });
 
